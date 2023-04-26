4f1acef2e9117fcbfda5d9ea3716fe479474d1d5
Nazar Vinnichuk <nazar@vinnich.uk>
2023-04-26 22:34:06 +0300

    Remove nix stuff.

--- lab-5/flake.nix
+++
@@ -1,6 +0,0 @@
-{
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = (import nixpkgs { inherit system; }).mkShell { };
-    });
-}
